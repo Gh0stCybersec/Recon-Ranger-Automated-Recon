6da4bd4eebb4a13b29b1155891549d764d331ea5
Daniel J <159965080+Gh0stCybersec@users.noreply.github.com>
2025-02-17 16:01:10 +0000

    Update recon.sh
    Slight update - removed pointless error check

--- recon.sh
+++ recon.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-baseDir="/change/to/target/directory"
+# Base Directory for root domains
+baseDir="${1:-/home/gh0st/Documents/BugBounty}"
 
 
 echo "______                         "
@@ -8,58 +9,57 @@ echo "| |_/ /___  ___ ___  _ __    "
 echo "|    // _ \/ __/ _ \|  _ \  "
 echo "| |\ \  __/ (_| (_) | | | | "
 echo "\_| \_\___|\___\___/|_| |_| Ranger Automated Recon"
-echo "Created by Gh0st"
+echo "Created by Daniel Jennings"
 echo "Github: https://github.com/Gh0stCybersec"
 
-# Check if the base directory exists
-if [[ ! -d "$baseDir" ]]; then
-    echo "Directory '$baseDir' does not exist."
-    exit 1
-fi
+if  [[ -d "$baseDir" ]]; then
+    for dir in "$baseDir"/*/; do    
+        if [[ -f "${dir}/roots.txt" ]]; then
+            programName=$(basename "$dir")
+            echo "$(date): Recon for $programName...." | notify
 
-# Check if required commands are available
-required_commands=("subfinder" "dnsx" "httpx" "smap" "nuclei" "anew" "altdns")
-missing_commands=()
-for cmd in "${required_commands[@]}"; do
-    if ! command -v "$cmd" &>/dev/null; then
-        missing_commands+=("$cmd")
-    fi
-done
+             # subdomain enumeration"
+            subfinder -dL "${dir}/roots.txt" | dnsx | anew -q "${dir}/resolveddomains.txt" || {
+                echo "$(date): Subdomain discovery failed for $programName"
+                continue
+            }
 
-if [[ ${#missing_commands[@]} -ne 0 ]]; then
-    echo "Error: The following required tools are not installed or not in your PATH:"
-    for missing_cmd in "${missing_commands[@]}"; do
-        echo "  - $missing_cmd"
+             # HTTP probing ensure to do sudo apt remove python3-httpx
+              httpx -l "${dir}/resolveddomains.txt" -t 75 | anew "${dir}/webservers.txt" | notify -bulk || {
+                echo "$(date): Http probing failed for $programName"
+                continue
+            } 
+              # Port Scanning
+              nmap -sS -sV -O -iL "${dir}/resolveddomains.txt" | anew -q "${dir}/openports.txt" | notify -bulk || {
+                echo "$(date): Port scanning failed for $programName"
+                continue
+            } 
+              # Vulnerability Scanning
+              nuclei -l "${dir}/resolveddomains.txt" -severity critical,high,medium | anew -q "${dir}/nucleiresults.txt" | notify  || {
+                echo "$(date): Nuclei scan failed for $programName"
+                continue
+            } 
+        else
+            echo  "$(data): No root domains found for $programName!"
+        fi
     done
-    exit 1
+else
+    echo "$(date): Directory '$baseDir' does not exist."
 fi
 
-# Iterate through subdirectories in the base directory
-for dir in "$baseDir"/*/; do
-    if [[ -f "${dir}/roots.txt" ]]; then
-        programName=$(basename "$dir")
-        echo "Recon for $programName:"
-        # Perform reconnaissance and vulnerability scanning
-        subfinder -dL "${dir}/roots.txt" | dnsx | anew "${dir}/resolveddomains.txt" || { echo "Error in subfinder or dnsx command"; exit 1; }
-        httpx -l "${dir}/resolveddomains.txt" -t 75 | anew "${dir}/webservers.txt" || { echo "Error in httpx command"; exit 1; }
-        smap -iL "${dir}/resolveddomains.txt" | anew "${dir}/openports.txt" || { echo "Error in smap command"; exit 1; }
-        #permutate the list of resolved domains using given list (permutations.txt)
-        altdns -i "${dir}/resolveddomains.txt" -o "${dir}/alt_output_dump" -w permutations.txt -r -s "${dir}/altresolveddomains.txt" | anew "${dir}/altresolveddomains.txt" || { echo "Error in altdns command"; exit 1; }
-        #perform run of nuclei and all default templates
-        nuclei -l "${dir}/resolveddomains.txt" -rl 5 -c 5 -o "${dir}/nuclei_results.txt" || { echo "Error in nuclei command"; exit 1; }
-    else
-        programName=$(basename "$dir")
-        echo "No root domains found for $programName!"
-    fi
-done
+
+#permutate the list of resolved domains using given list (permutations.txt)
+        
+        #altdns -i "${dir}/resolveddomains.txt" -o "${dir}/alt_output_dump" -w permutations.txt -r -s "${dir}/altresolveddomains.txt" | anew "${dir}/altresolveddomains.txt" 
 
 
-# to do 
-# add amass?
-# add nuclei templates 
-# directory brute force - for loop , loop each line in domains.txt and send to gobuster
-# add gungir run? 
 
-   
+##add run on katana? 
+#add gungir run?
 
-   
+
+
+
+
+
+
